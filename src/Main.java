1a824110b4698d39db4a83f14e048b36ec7a9bf4
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-05-28 21:17:03 +0200

    Added tests for score

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 import flappyBird.*;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        var frame = new GameFrame();
+        String playerName = JOptionPane.showInputDialog("Enter name: ");
+        Player player = new Player(playerName);
+        var frame = new GameFrame(player);
         frame.startGame();
     }
+    /*
+    public static void main(String[] args) {
+        Score score = new Score();
+        System.out.println(score);
+        score.insert(3, "Maciek");
+        System.out.println(score);
+        score.insert(1, "CEP");
+        System.out.println(score);
+        score.insert(12, "CEP");
+        System.out.println(score);
+    }
+    */
 }
