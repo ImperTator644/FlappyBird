a2e6a492f8641f8b61ea49cfe1bb1e33c500fdf4
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-05-28 21:15:32 +0200

    Extracted methods and variables common for player

--- src/flappyBird/GamePanel.java
+++ src/flappyBird/GamePanel.java
@@ -1,13 +1,10 @@
 package flappyBird;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 import java.util.Random;
 
 
@@ -19,13 +16,16 @@ public class GamePanel extends JLabel implements ActionListener {
 
     private GamePanel.Movement movement = GamePanel.Movement.NONE;
 
+    private final Player player;
+    private final Score highScores;
     protected static final int SCREEN_WIDTH = 500;
     protected static final int SCREEN_HEIGHT = 700;
     protected static final int UNIT_SIZE = 10;
+    protected static final int PLAYER_SIZE = 3 * UNIT_SIZE;
     protected static final int GAP = 10 * UNIT_SIZE;
     protected static final int MOVE_TIMER_DELAY = 100;
-    protected static final int FLY_TIMER_DELAY = 10;
-    protected static final int TIMER_DELAY = 25;
+    protected static final int FLY_TIMER_DELAY = 20;
+    protected static final int TIMER_DELAY = 50;
     private int obstaclePosX            = 0;
     private int obstaclePosY            = 0;
     private int obstaclesPassed         = 0;
@@ -33,27 +33,19 @@ public class GamePanel extends JLabel implements ActionListener {
     private boolean jump                = false;
     private boolean restart             = false;
     private boolean exit                = false;
-    private Image player;
-    static final int PLAYERX            = 100;
-    int playerY = SCREEN_HEIGHT / 2;
-    Timer gameTimer;
-    Timer jumpTimer;
-    Timer flyTimer;
-    Random random;
+    private final boolean isImage;
+    private Timer gameTimer;
+    private Timer jumpTimer;
+    private Timer flyTimer;
+    private final Random random;
 
-    public GamePanel(String s, ImageIcon background, int center) {
+    public GamePanel(String s, ImageIcon background, int center, Player player) {
         super(s,background,center);
+        this.player = player;
+        this.highScores = new Score();
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-        loadImage();
-    }
-
-    protected void loadImage(){
-        try{
-            player = ImageIO.read(new File("src/images/bird.png"));
-        }catch (IOException e){
-            System.out.println(e.getMessage());
-        }
+        isImage = player.loadImage();
     }
 
     public void initGame() {
@@ -63,11 +55,11 @@ public class GamePanel extends JLabel implements ActionListener {
 
         gameTimer = new Timer(TIMER_DELAY, e -> {
             if (!restart) {
-                if (!jump) playerY += UNIT_SIZE;
-                if (playerY < UNIT_SIZE) {
-                    playerY = UNIT_SIZE;
-                } else if (playerY > SCREEN_HEIGHT - UNIT_SIZE) {
-                    playerY = SCREEN_HEIGHT - UNIT_SIZE;
+                if (!jump) player.addPlayerY(UNIT_SIZE);
+                if (player.getPlayerY() < UNIT_SIZE) {
+                    player.setPlayerY(UNIT_SIZE);
+                } else if (player.getPlayerY() > SCREEN_HEIGHT - UNIT_SIZE) {
+                    player.setPlayerY(SCREEN_HEIGHT - UNIT_SIZE);
                 }
                 if (running) {
                     obstacleMove();
@@ -77,7 +69,7 @@ public class GamePanel extends JLabel implements ActionListener {
                 obstaclesPassed = 0;
                 running = true;
                 restart = false;
-                playerY = SCREEN_HEIGHT / 2;
+                player.setPlayerY(SCREEN_HEIGHT / 2);
                 newObstacle();
             }
             repaint();
@@ -85,7 +77,7 @@ public class GamePanel extends JLabel implements ActionListener {
         jumpTimer = new Timer(MOVE_TIMER_DELAY, this);
 
         flyTimer = new Timer(FLY_TIMER_DELAY, e -> {
-            if (jump) playerY -= UNIT_SIZE;
+            if (jump) player.decPlayerY(UNIT_SIZE);
             repaint();
         });
     }
@@ -124,15 +116,17 @@ public class GamePanel extends JLabel implements ActionListener {
     private void draw(Graphics g) {
         if (running) {
             g.setColor(Color.red);
-            g.drawImage(player,PLAYERX,playerY,2*UNIT_SIZE,2*UNIT_SIZE,this);
+            if(isImage) g.drawImage(player.getImage(),player.getPLAYERX(),player.getPlayerY(),PLAYER_SIZE,PLAYER_SIZE,this);
+            else g.fillRect(player.getPLAYERX(), player.getPlayerY(), PLAYER_SIZE, PLAYER_SIZE);
             
             g.setColor(new Color(34,69,6));
             g.fillRect(obstaclePosX, 0, UNIT_SIZE * 2, obstaclePosY);
             g.fillRect(obstaclePosX, obstaclePosY + GAP, UNIT_SIZE * 2, SCREEN_HEIGHT);
 
             g.setColor(Color.red);
-            g.setFont(new Font("Ink Free", Font.BOLD, 30));
+            g.setFont(new Font("Ink Free", Font.BOLD, 25));
             FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Player: " + player.getName(), 0, g.getFont().getSize());
             g.drawString("Score: " + obstaclesPassed, (SCREEN_WIDTH - metrics.stringWidth("Score: " + obstaclesPassed)), g.getFont().getSize());
         } else {
             gameOver(g);
@@ -173,10 +167,10 @@ public class GamePanel extends JLabel implements ActionListener {
     }
 
     private void checkCollisions() {
-        if (obstaclePosX == PLAYERX
-                || obstaclePosX + UNIT_SIZE == PLAYERX
-                || obstaclePosX + 2 * UNIT_SIZE == PLAYERX) {
-            if (playerY < obstaclePosY || playerY + UNIT_SIZE > obstaclePosY + GAP) running = false;
+        if (obstaclePosX == player.getPLAYERX()
+                || obstaclePosX + UNIT_SIZE == player.getPLAYERX()
+                || obstaclePosX + 2 * UNIT_SIZE == player.getPLAYERX()) {
+            if (player.getPlayerY() < obstaclePosY || player.getPlayerY() + PLAYER_SIZE > obstaclePosY + GAP) running = false;
         }
     }
 
