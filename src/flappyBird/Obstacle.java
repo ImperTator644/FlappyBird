dd5a392b913ff2fd3b327e86686a4f9fb1ed3878
Konrad Blicharz <konradknight19@gmail.com>
2022-05-31 19:28:24 +0200

    New class for Obstacle

---
+++ src/flappyBird/Obstacle.java
@@ -0,0 +1,31 @@
+package flappyBird;
+
+public class Obstacle {
+    private int obstaclePosX;
+    private int obstaclePosY;
+
+    public Obstacle() {
+        this.obstaclePosX = 0;
+        this.obstaclePosY = 0;
+    }
+
+    public int getObstaclePosX() {
+        return obstaclePosX;
+    }
+
+    public int getObstaclePosY() {
+        return obstaclePosY;
+    }
+
+    public void setObstaclePosX(int obstaclePosX) {
+        this.obstaclePosX = obstaclePosX;
+    }
+
+    public void setObstaclePosY(int obstaclePosY) {
+        this.obstaclePosY = obstaclePosY;
+    }
+
+    public void decObstacleX(int unit){
+        this.obstaclePosX -= unit;
+    }
+}
