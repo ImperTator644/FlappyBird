2d6c58c63c8173b2b5a1ad1c5ff713d4d0f41b41
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-06-03 10:28:35 +0200

    Added color picking

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-import flappyBird.*;
+import flappyBird.Bird;
+import flappyBird.GameFrame;
+import flappyBird.Player;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
         String playerName = JOptionPane.showInputDialog("Enter name: ");
-        Player player = new Player(playerName);
+        String color = JOptionPane.showInputDialog("Enter color {yellow, red, green}: ");
+        Player player = new Bird(playerName, color);
         var frame = new GameFrame(player);
         frame.startGame();
     }
