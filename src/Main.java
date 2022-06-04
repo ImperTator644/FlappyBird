5904c49b10699e095f60349fd538663b77782de5
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-06-04 21:43:16 +0200

    Picking color

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import flappyBird.GameFrame;
 import flappyBird.Player;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
