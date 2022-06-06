4066b06071793fc4bd29ec82749ff728c727e423
Konrad Blicharz <konradknight19@gmail.com>
2022-06-06 20:07:01 +0200

    nice

--- src/flappyBird/GameFrame.java
+++ src/flappyBird/GameFrame.java
@@ -5,9 +5,10 @@ import javax.swing.*;
 public class GameFrame extends JFrame {
     private final GamePanel panel;
 
-    public GameFrame(Player player, Info info) {
+    public GameFrame(Player player, Info info,String color) {
         ImageIcon background = new ImageIcon("src/images/background.png");
         ImageIcon icon = new ImageIcon("src/images/birdFlyYellow.png");
+        if(color.equals("beer")) background = new ImageIcon("src/images/backgroundTwo.png");
         panel = new GamePanel("", background, JLabel.CENTER, player, info);
         this.add(panel);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
